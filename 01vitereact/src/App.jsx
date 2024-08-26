7c983cf8259a897aafcb2d0f82dde60afe034c9f
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-08-26 05:59:27 +0000

    react.js

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 import User from "./01"
 
 function App() {
-
+const username="Izza Shahzad"
   return (
     <>
     <User/>
-    <h1>Hello Sunshine</h1>
+    <h1>Hello Sunshine {username}</h1>
     <p>test paragrapgh</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://youtube.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Youtube'
+// }
+/*const anotherElement = (
+  <a href="https://youtube.com" target='_blank'>Visit Youtube</a>
+)
+  const anotherUser = "hello"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://youtube.com',target: '_blank' },
+    'click me to visit Youtube',
+    anotherElement
+)
+    ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    reactElement
+  
+)
+*/
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
