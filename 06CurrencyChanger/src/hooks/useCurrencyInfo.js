21a2a7514729c207cba2236f969f7c6c11f192a3
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-08-28 07:34:00 +0000

    Currency Converter Project

---
+++ 06CurrencyChanger/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://v6.exchangerate-api.com/v6/ee30a36c9dde4ec81d52dd0b/latest/USD/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
